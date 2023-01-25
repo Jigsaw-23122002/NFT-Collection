10a70fc62170d2be41f3067e7112864d22cf1f85
Harsh <harshnag23@gmail.com>
2023-01-25 21:56:58 +0530

    Final deployment

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xd4400B33C23DE3c86E8c09063252fdF5955a86cd";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "";
+const METADATA_URL = "https://nft-collection-theta-six.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
