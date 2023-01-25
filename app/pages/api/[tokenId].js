f481ddd74bef4bd45a6fc766fb5aa348dff11804
Harsh <harshnag23@gmail.com>
2023-01-25 21:50:17 +0530

    Deployment push

--- app/pages/api/[tokenId].js
+++ app/pages/api/[tokenId].js
@@ -2,7 +2,14 @@
 
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
-  res.status(200).json({
-    tokenId: tokenId,
+  const name = `Crypto Dev #${tokenId}`;
+  const description = "CryptoDevs is an NFT Collection for Web3 Developers";
+  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${
+    tokenId - 1
+  }.svg`;
+  return res.json({
+    name: name,
+    description: description,
+    image: image,
   });
 }
