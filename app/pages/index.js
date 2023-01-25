dd4590e18b2230486ef8dce8b33f873e6f5a9c90
Harsh <harshnag23@gmail.com>
2023-01-25 19:43:04 +0530

    Presale is not getting started

---
+++ app/pages/index.js
@@ -0,0 +1,247 @@
+import { useEffect, useRef, useState } from "react";
+import Head from "next/head";
+import { Contract, providers, utils } from "ethers";
+import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { NFT_CONTRACT_ABI, NFT_CONTRACT_ADDRESS } from "../constants";
+
+export default function Home() {
+  const [isOwner, setIsOwner] = useState(false);
+  const [presaleStarted, setPresaleStarted] = useState(false);
+  const [presaleEnded, setPresaleEnded] = useState(false);
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const web3ModalRef = useRef();
+
+  const presaleMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        signer
+      );
+      const txn = await nftContract.presaleMint({
+        value: utils.parseEther("0.01"),
+      });
+      await txn.wait();
+      window.alert("You successfully minted a CryptoDev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const publicMint = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        signer
+      );
+      const txn = await nftContract.mint({
+        value: utils.parseEther("0.01"),
+      });
+      await txn.wait();
+      window.alert("You successfully minted a CryptoDev!");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getOwner = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        signer
+      );
+      const owner = await nftContract.owner();
+      const userAddress = await signer.getAddress();
+      if (owner.toLowerCase() === userAddress.toLowerCase()) {
+        setIsOwner(true);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const startPresale = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        signer
+      );
+      const txn = await nftContract.startPresale();
+      setLoading(true);
+      await txn.wait();
+      setLoading(false);
+      setPresaleStarted(true);
+      await checkIfPresaleStarted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const checkIfPresaleEnded = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+
+      const presaleEndTime = await nftContract.presaleEnded();
+      const currentTimeInSeconds = Date.now();
+      const hasPresaleEnded = presaleEndTime.lt(
+        Math.floor(currentTimeInSeconds)
+      );
+      setPresaleEnded(hasPresaleEnded);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const checkIfPresaleStarted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        provider
+      );
+
+      const isPresaleStarted = await nftContract.presaleStarted();
+      setPresaleStarted(isPresaleStarted);
+      return isPresaleStarted;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  };
+
+  const connectWallet = async () => {
+    await getProviderOrSigner();
+    setWalletConnected(true);
+  };
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 5) {
+      window.alert("Please switch to the Goerli network");
+      throw new Error("Incorrect network");
+    }
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+  };
+
+  const onPageLoad = async () => {
+    await connectWallet();
+    await getOwner();
+    const presaleStarted = await checkIfPresaleStarted();
+    if (presaleStarted) {
+      await checkIfPresaleEnded();
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      onPageLoad();
+    }
+  });
+
+  function renderBody() {
+    if (!walletConnected) {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          {" "}
+          Connect your Wallet
+        </button>
+      );
+    }
+    if (loading) {
+      return <span className={styles.description}>Loading...</span>;
+    }
+    if (isOwner && !presaleStarted) {
+      return (
+        <button onClick={startPresale} className={styles.button}>
+          {" "}
+          Start Presale
+        </button>
+      );
+    }
+    if (!presaleStarted) {
+      return (
+        <div>
+          <span className={styles.description}>
+            Presale has not started yet. Come back later!
+          </span>
+        </div>
+      );
+    }
+    if (presaleStarted && !presaleEnded) {
+      return (
+        <div>
+          <span className={styles.description}>
+            Presale has started! If your address is whitelisted, you can mint a
+            CrytoDev!
+          </span>
+          <button className={styles.button} onClick={presaleMint}>
+            Presale Mint
+          </button>
+        </div>
+      );
+    }
+    if (presaleEnded) {
+      return (
+        <div>
+          <span className={styles.description}>
+            Presale has ended. You can mint a CryptoDev in public sale, if any
+            remain.
+          </span>
+          <button className={styles.button} onClick={publicMint}>
+            Public Mint
+          </button>
+        </div>
+      );
+    }
+    return <div></div>;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Crypto Devs NFT</title>
+      </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to CryptoDevs NFT</h1>
+          <span className={styles.description}>
+            CryptoDevs NFT is a collection for developers in web3
+          </span>
+          {renderBody()}
+        </div>
+        <img className={styles.image} src="/cryptodevs/crypto-devs.svg" />
+      </div>
+      <footer className={styles.footer}>
+        Made with &#10084; by Crypto Devs
+      </footer>
+    </div>
+  );
+}
--- hardhat-tutorial/contracts/IWhitelist.sol
+++ hardhat-tutorial/contracts/IWhitelist.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.4;
 
-// Interfaces are used to just show the function definations that our contract is going to use.
 interface IWhitelist {
-    function whitelistAddresses(address) external view returns (bool);
+    function whitelistedAddresses(address) external view returns (bool);
 }
